2b466ff461185e8ed127b2c0f77ac5bff3ea1574
Sreejith Mohanan <sree.rfx@gmail.com>
2018-05-03 19:15:16 +0530

    completed tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,5 +14,32 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
   TODO:
     * Calculate the RMSE here.
-  */
+    */
+
+	  VectorXd rmse(4);
+		rmse << 0,0,0,0;
+
+		// assert the following inputs:
+		// estimation vector size should not be zero
+		// estimation vector size should equal ground truth vector size
+		if(estimations.size() != ground_truth.size()
+				|| estimations.size() == 0){
+			std::cout << "Invalid estimation or ground_truth data" <<std::endl;
+			return rmse;
+		}
+
+		//accumulate squared difference
+		for(uint8_t i=0; i < estimations.size(); ++i){
+			VectorXd difference = estimations[i] - ground_truth[i];
+
+			//coefficient-wise multiplication
+			difference = difference.array()*difference.array();
+			rmse += difference;
+		}
+
+		//find the mean and squared root of mean
+		rmse = rmse/estimations.size();
+		rmse = rmse.array().sqrt();
+	  return rmse;
+
 }
